b6c190f15a84995078538a63982d6ffa9a6126ed
Vujinović,Stefan S <stefan.vujinovic@student.fontys.nl>
2019-09-10 14:17:58 +0200

    Start screen updated

--- src/App.js
+++ src/App.js
@@ -7,21 +7,19 @@ class App extends Component {
             <body>
             <div className="App">
                 <div class="topnav">
-                    <a href="#">Link</a>
-                    <a href="#">Link</a>
-                    <a href="#">Link</a>
+                    <a href="#">Login</a>
+                    <a href="#">Contact</a>
+                    <a href="#">About</a>
+                </div>
+                <div className="content">
+                    <div className="title1">BuddyFinder</div>
+                    <div className="titletext"><center><p>Find buddies for your favorite activities!</p></center></div>
+                    <div className="button1">Get Started ></div>
                 </div>
 
-                <div class="content">
-                    <h1>BuddyFinder</h1>
-                    <p>Find buddies for your favorite activities!</p>
-                </div>
-
-                <div class="footer">
+                <div className="footer">
                     <div class="footertext">
-                       
                         <p>Created by Stefan Vujinovic - Official licenced product by Microsoft</p>
-            
                     </div>
                 </div>
             </div>
