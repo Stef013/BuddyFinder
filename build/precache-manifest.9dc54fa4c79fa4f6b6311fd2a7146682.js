4f85b1f72bdeb2440524d1c6b533a03c4a853d94
Vujinović,Stefan S <stefan.vujinovic@student.fontys.nl>
2020-01-15 16:31:30 +0100

    build update

---
+++ build/precache-manifest.9dc54fa4c79fa4f6b6311fd2a7146682.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f21555b4b2c8e43136a1665d36d89ab1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59a71084aacf938cdbb0",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "f2a21350330e02099d56",
+    "url": "/static/css/main.0e5396d4.chunk.css"
+  },
+  {
+    "revision": "59a71084aacf938cdbb0",
+    "url": "/static/js/2.56eeae79.chunk.js"
+  },
+  {
+    "revision": "c9ddc7aaf14eb06fbd663cecc517100b",
+    "url": "/static/js/2.56eeae79.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f2a21350330e02099d56",
+    "url": "/static/js/main.b7bcad2b.chunk.js"
+  },
+  {
+    "revision": "fd7f8a8b9ca85124dc76",
+    "url": "/static/js/runtime-main.a613269b.js"
+  },
+  {
+    "revision": "5d3998ebebb0d15d5ce07bbc8fab9d8c",
+    "url": "/static/media/happypeople2.5d3998eb.jpg"
+  }
+]);
